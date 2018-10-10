135601993032abd01b5d487f0d519e79004709c0
Hannah Vaughan <vaughanh@oregonstate.edu>
2018-10-10 16:38:14 -0700

    Fix constructor bugs Caius found in Ship and Square classes

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -2,14 +2,15 @@ package cs361.battleships.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
 	
-	public Ship(String kind) {
-		//TODO implement
+	public Ship() {
+		occupiedSquares = new ArrayList<>();
 	}
 
 	public List<Square> getOccupiedSquares() {
