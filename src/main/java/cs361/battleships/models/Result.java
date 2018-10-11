8a777a7e103660f2eba6292e7a4768214fc25b4b
Hannah Vaughan <vaughanh@oregonstate.edu>
2018-10-10 22:44:13 -0700

    Change whitespace in Result class

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -7,27 +7,15 @@ public class Result {
 	@JsonProperty private Ship ship;
 	@JsonProperty private Square location;
 
-	public AtackStatus getResult() {
-		return result;
-	}
+	public AtackStatus getResult() { return result; }
 
-	public void setResult(AtackStatus result) {
-		this.result = result;
-	}
+	public void setResult(AtackStatus result) { this.result = result; }
 
-	public Ship getShip() {
-		return ship;
-	}
+	public Ship getShip() { return ship; }
 
-	public void setShip(Ship ship) {
-		this.ship = ship;
-	}
+	public void setShip(Ship ship) { this.ship = ship; }
 
-	public Square getLocation() {
-		return location;
-	}
+	public Square getLocation() { return location; }
 
-	public void setLocation(Square square) {
-		location = square;
-	}
+	public void setLocation(Square square) { location = square; }
 }
